405b6a287c2e4a9240e8fb39d9080cfca91f44d1
Onur Yıldırım <onur@cutepilot.com>
2017-08-24 05:12:08 +0300

    add jasmine runner, remove grunt

---
+++ test/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/* eslint camelcase:0 no-multi-spaces:0 */
+
+// setup Jasmine
+const Jasmine = require('jasmine');
+const jasmine = new Jasmine();
+jasmine.loadConfig({
+    spec_dir: 'test',
+    spec_files: ['**/*[sS]pec.js'],
+    helpers: ['helpers/**/*.js'],
+    random: false,
+    seed: null,
+    stopSpecOnExpectationFailure: false
+});
+jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+
+// setup console reporter
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const reporter = new JasmineConsoleReporter({
+    colors: 1,           // (0|false)|(1|true)|2
+    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    verbosity: 4,        // (0|false)|1|2|(3|true)|4
+    listStyle: 'flat',   // "flat"|"indent"
+    activity: false
+});
+
+// initialize and execute
+jasmine.env.clearReporters();
+jasmine.addReporter(reporter);
+jasmine.execute();
